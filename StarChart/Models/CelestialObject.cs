dc50e6b9696733d1ef3742a65a7b89f60fc7a81e
bakester <bakester17@gmail.com>
2022-06-26 14:18:55 -0400

    NotMapped fix for satellite list

--- StarChart/Models/CelestialObject.cs
+++ StarChart/Models/CelestialObject.cs
@@ -7,16 +7,19 @@ namespace StarChart.Models
 {
     public class CelestialObject
     {
-        //ID used to 
+        //ID for verification
         public int Id { get; set; }
 
+        //Set name, with required attr, so that there's no empty obj
         [Required(ErrorMessage = "Name is required")]
         public string Name { get; set; }
 
-        //Create nullable int for ID of Object "OrbitedObject"
+        //Create nullable int for ID of Object "OrbitedObject", must allow null vals for user err/to catch
         public int? OrbitedObjectId { get; set; }
 
-        //List of type CelestialObject that contains Satellites
+        //List of type CelestialObject that contains Satellites. 
+        //Notmapped attr. as Satellites does not need to be created in the db/mapped to col
+        [NotMapped]
         public List<CelestialObject> Satellites { get; set; }
 
         public TimeSpan OrbitalPeriod { get; set; }
