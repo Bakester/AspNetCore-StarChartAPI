3ec47b5ac540ed8d013b52b22d4d27ae5901795e
bakester <bakester17@gmail.com>
2022-06-26 17:21:36 -0400

    This is why you don't copy paste your methods

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -68,14 +68,6 @@ namespace StarChart.Controllers
             return Ok(celestialObjects);
         }
 
-        [HttpPost]
-        public IActionResult Create([FromBody]CelestialObject celestialObject)
-        {
-            _context.CelestialObjects.Add(celestialObject);
-            _context.SaveChanges();
-
-            return CreatedAtRoute("GetById", new { id = celestialObject.Id }, celestialObject);
-        }
 
         [HttpPost]
         public IActionResult Create([FromBody] CelestialObject celestialObject)
