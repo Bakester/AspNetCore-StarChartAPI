37d9648700217795df771147113af289174fe71d
bakester <bakester17@gmail.com>
2022-06-26 15:10:53 -0400

    Controller for Celestial Objects

---
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StarChart.Data;
+
+//Object Controller that contains ApiController attr. as well as a route TBD
+namespace StarChart.Controllers
+{
+    [ApiController]
+    [Route("")]
+    public class CelestialObjectController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        //CelestialObjectController constructor to set val w/ private readonly _context
+        public CelestialObjectController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+
+    }
+}
